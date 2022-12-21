8b0d03884ffb09e861b54ea6ea2bf62f82c8bb4b
emcutene <117540926+emcutene@users.noreply.github.com>
2022-12-21 09:39:24 +0700

    dat commit

---
+++ Core/Inc/fsm_buttons.h
@@ -0,0 +1,14 @@
+/*
+ * fms_buttons.h
+ *
+ *  Created on: Dec 8, 2022
+ *      Author: Dattt
+ */
+
+#ifndef INC_FSM_BUTTONS_H_
+#define INC_FSM_BUTTONS_H_
+
+
+void fsm_simple_buttons_run ();
+
+#endif /* INC_FSM_BUTTONS_H_ */
